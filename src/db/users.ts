82b565c10be378bf60016f1d68743f8765f2bb55
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-05-10 20:13:05 -0300

    feat: user

--- src/db/users.ts
+++ src/db/users.ts
@@ -14,5 +14,12 @@ export const UserModel = mongoose.model("User", UserSchema);
 
 export const gerUser = () => UserModel.find();
 export const gerUserByEmail = (email: string) => UserModel.findOne({ email });
-export const gerUserByEmailToken = (email: string) =>
-  UserModel.findOne({ email });
+export const gerUserByEmailToken = (sessionToken: string) =>
+  UserModel.findOne({ "authentication.sessionToiken": sessionToken });
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) =>
+  new UserModel(values).save();
+export const deleteUserById = (id: string) =>
+  UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) =>
+  UserModel.findOneAndUpdate({ id, values });
