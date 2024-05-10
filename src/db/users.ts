3cdb0b6a31de1e057d4dbc41be2d21482bd0a849
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-05-10 19:48:17 -0300

    feat:  autenticacao

--- src/db/users.ts
+++ src/db/users.ts
@@ -4,6 +4,8 @@ const UserSchema = new mongoose.Schema({
   userName: { type: String, required: true },
   email: { type: String, required: true },
   authentication: {
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false },
+    salt: { type: String, required: true },
+    sessionToken: { type: String, required: true },
   },
 });
