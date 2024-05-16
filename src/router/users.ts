1e5f316992bff3f6fd71dac20897d529a040f1bf
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-05-15 22:21:11 -0300

    feat: update controller

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,8 +1,9 @@
 import express from "express";
 
 import { deleteUser, getAllUsers } from "../controllers/users";
+import { isAuthenticated, isOwner } from "middlewares";
 
 export default (router: express.Router) => {
-  router.get("/users", getAllUsers);
-  router.delete("/users/:id", deleteUser);
+  router.get("/users", getAllUsers, isAuthenticated);
+  router.delete("/users/:id", isAuthenticated, isOwner, deleteUser);
 };
